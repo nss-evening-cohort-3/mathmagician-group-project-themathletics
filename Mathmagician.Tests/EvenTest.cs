3e4e2ccd223ee6adcdaf0dfa9798b5fd87a553ff
Dan Ventura <danwventura@icloud.com>
2016-08-04 19:46:16 -0500

    adding basic user prompts, two even test, and even for loop

---
+++ Mathmagician.Tests/EvenTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Mathmagician.Tests
+{
+    [TestClass]
+    public class EvenTest
+    {
+        
+
+        [TestMethod]
+        public void GiveBackEvensList()
+        {
+            // Arrange section
+            Even myEven = new Even();
+            // Act
+            var evensTestList = myEven.CreateEvenSequence(5);
+            // Assert
+            Assert.IsInstanceOfType(evensTestList, typeof(List<int>));
+        }
+
+        [TestMethod]
+        public void GivesBackRightNumberOfEvens()
+        {
+            // Arrange
+            Even myEven = new Even();
+            // Act
+           var evensTestList = myEven.CreateEvenSequence(5);
+            // Assert
+            Assert.AreEqual(evensTestList.Count, 5);
+
+        }
+    }
+}
--- Mathmagician/Program.cs
+++ Mathmagician/Program.cs
@@ -10,6 +10,14 @@ namespace Mathmagician
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What would you like for me to do?");
+            String user_command = Console.ReadLine().ToLower() + " ";
+            Console.WriteLine("How many should I print?");
+            string user_number = Console.ReadLine() + " ";
+            Console.WriteLine("Cool, I'm going to print " + user_number + user_command + "numbers.");
+
+            Console.ReadLine();
+
         }
     }
 }
